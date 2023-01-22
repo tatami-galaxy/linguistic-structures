68b0535b4d33aef3ed39d71c0471ae93f0ba0223
ujan <ujandev@gmail.com>
2023-01-23 02:22:49 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='identifying and leveraging linguistic structures formed within language models',
+    author='ujan',
+    license='MIT',
+)
