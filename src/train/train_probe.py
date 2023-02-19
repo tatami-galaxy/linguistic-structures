dae0b03abaf226dbdaa64b0be8d54cc148588e81
ujan <ujandev@gmail.com>
2023-02-19 16:21:06 +0000

    debug

--- src/train/train_probe.py
+++ src/train/train_probe.py
@@ -574,7 +574,7 @@ if __name__ == '__main__':
                 # loss
                 val_loss += l1(pred_dist, labels, label_mask, lens).item()
 
-        print('val loss : {}'.format(val_loss/eval_dataloader))
+        print('val loss : {}'.format(val_loss/len(eval_dataloader)))
 
 
 
