d1df9da0f89abab5b941c79bc1c18b863c968d78
ujan <ujandev@gmail.com>
2023-03-14 15:43:32 +0530

    repo setup code

---
+++ repo_setup.py
@@ -0,0 +1,12 @@
+import os
+
+os.mkdir('data')
+os.mkdir('data/raw')
+os.mkdir('data/processed')
+os.mkdir('data/interim')
+os.mkdir('data/processed/UD')
+os.mkdir('data/processed/wikiann')
+
+os.mkdir('models')
+os.mkdir('models/probes')
+os.mkdir('models/finetuned')
